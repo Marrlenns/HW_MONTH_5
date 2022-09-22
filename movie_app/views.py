95fd8ed2293769eae7e8dabe6b5c978eeaaa4964
Marrlenns <marlen.ormonbaev05@gmail.com>
2022-09-22 23:47:21 +0600

    find my stupid mistake

--- movie_app/views.py
+++ movie_app/views.py
@@ -16,7 +16,7 @@ def directors_view(request):
 @api_view(['GET'])
 def one_director_view(request, id):
     director = {
-        'name': Director.objects.get(id=id).name if Director.objects.count() > id else f"Режиссера под номером {id} еще не существует!!!"
+        'name': Director.objects.get(id=id).name if Director.objects.count() >= id else f"Режиссера под номером {id} еще не существует!!!"
     }
     return Response(data=director)
 
@@ -31,10 +31,10 @@ def movies_view(request):
 @api_view(['GET'])
 def one_movie_view(request, id):
     movie = {
-        'title': Movie.objects.get(id=id).title if Movie.objects.count() > id else f"Фильм №{id} еще не добавили в кинотеатр",
-        'duration': f"{Movie.objects.get(id=id).duration} мин" if Movie.objects.count() > id else f"У несуществующего фильма №{id} еще нету длительности",
-        'description': Movie.objects.get(id=id).description if Movie.objects.count() > id else f"У несуществующего фильма №{id} еще нету описания",
-        'director': Movie.objects.get(id=id).director.name if Movie.objects.count() > id else f"Так как не существует фильма №{id}, соответственно у нее и режиссера нет💁‍♂️",
+        'title': Movie.objects.get(id=id).title if Movie.objects.count() >= id else f"Фильм №{id} еще не добавили в кинотеатр",
+        'duration': f"{Movie.objects.get(id=id).duration} мин" if Movie.objects.count() >= id else f"У несуществующего фильма №{id} еще нету длительности",
+        'description': Movie.objects.get(id=id).description if Movie.objects.count() >= id else f"У несуществующего фильма №{id} еще нету описания",
+        'director': Movie.objects.get(id=id).director.name if Movie.objects.count() >= id else f"Так как не существует фильма №{id}, соответственно у нее и режиссера нет💁‍♂️",
     }
     return Response(data=movie)
 
@@ -49,7 +49,7 @@ def reviews_view(request):
 @api_view(['GET'])
 def one_review_view(request, id):
     review = {
-        'text': Review.objects.get(id=id).text if Review.objects.count() > id else f"Так как не существует фильма №{id}, ему нельзя оставить отзыв",
-        'movie': Review.objects.get(id=id).movie.title if Review.objects.count() > id else f"Фильма под номером {id} еще не добавили в кинотеатр"
+        'text': Review.objects.get(id=id).text if Review.objects.count() >= id else f"Так как не существует фильма №{id}, ему нельзя оставить отзыв",
+        'movie': Review.objects.get(id=id).movie.title if Review.objects.count() >= id else f"Фильма под номером {id} еще не добавили в кинотеатр"
     }
     return Response(data=review)
